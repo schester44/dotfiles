66a56708e23f3feedac1cd2709cad59d1783c305
Steve Chester <schester44@gmail.com>
2022-03-25 17:36:09 -0400

    brew config

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -14,12 +14,13 @@ brew install --cask 1password-cli
 brew install --cask firefox-developer-edition
 brew install --cask google-chrome
 brew install --cask spotify
+brew install --cask bitwarden
 
 ## Dev apps
 brew install --cask iTerm2
 brew install --cask postman
 brew install --cask visual-studio-code
-
+brew install --cask tableplus
 
 # Launchd configuration
 brew services start postgresql
---
+++ things-to-backup.md
@@ -0,0 +1,10 @@
+# Things to backup
+
+a guide to backup/move everything to a new laptop
+
+
+- ~/.dotfiles
+  - ensure committed and pushed
+- ~/work
+  - ensure important directories are commited and pushed
+- ~/.ssh
