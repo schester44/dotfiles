570577247bceed8743083be9a0508de1fca6f155
Steve Chester <schester44@gmail.com>
2022-12-23 12:00:54 -0500

    add git recent command

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+git config --global alias.upstream '!git push -u origin HEAD'
+git config --global alias.recent 'branch --sort=-committerdate --format="%(committerdate:relative)%09%(refname:short)"'
+
+
 alias gpoh="git push origin head"
-alias git-date='git for-each-ref --sort=-committerdate refs/heads/ | head -n 50'
 alias git-rm-branches="git-date | awk '{print $3}' | sed -e "s/^refs\/heads\///" | awk '$0 !~ /dev|master/' | xargs git branch -D"
 alias gcd="git checkout dev"
 alias gmd="git merge dev"
