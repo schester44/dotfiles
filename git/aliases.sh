8e6dbd92db947cb1a174358718bcca0cdd9e9246
Steve Chester <schester44@gmail.com>
2024-11-26 00:08:25 -0500

    git merge last

--- git/aliases.sh
+++ git/aliases.sh
@@ -10,12 +10,14 @@ git_set_upstream() {
 
 git config --global alias.upstream '!git push -u origin HEAD'
 git config --global alias.recent 'branch --sort=-committerdate --format="%(committerdate:relative)%09%(refname:short)"'
+git config --global alias.ml 'merge @{-1}'
 
 alias gpsu="git_set_upstream"
 alias gpoh="git push origin head"
 alias git-rm-branches="git-date | awk '{print $3}' | sed -e "s/^refs\/heads\///" | awk '$0 !~ /dev|master/' | xargs git branch -D"
 alias gcm="git checkout main"
 alias gmm="git merge main"
+alias gml="git merge @{-1}"
 alias gs="git status"
 alias gpm="git pull origin main"
 alias glast="git checkout @{-1}"
