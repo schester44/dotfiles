b5f7d0372b52ca566fae251521103576938c959f
Steve Chester <schester44@gmail.com>
2024-12-02 15:51:23 -0500

    tmux and wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,8 +8,9 @@ config.colors = {
 	background = "#1C2E41",
 }
 
-config.font = wezterm.font("Operator Mono Lig", { weight = 325 })
-config.font_size = 13
+config.font = wezterm.font("Operator Mono Lig", { weight = 345 })
+config.font_size = 14
+config.line_height = 1.1
 
 config.enable_tab_bar = false
 
