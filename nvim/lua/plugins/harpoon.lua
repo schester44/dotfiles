bb890a4a7601facdcdba3cd7be58d11ce99e2967
Steve Chester <schester44@gmail.com>
2024-12-04 16:20:32 -0500

    harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,9 +7,29 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup {}
 
+    local function git_diff()
+      local pickers = require 'telescope.pickers'
+      local finders = require 'telescope.finders'
+      local conf = require('telescope.config').values
+      local list = vim.fn.systemlist 'git diff --name-only'
+
+      pickers
+        .new({}, {
+          prompt_title = 'git diff',
+          finder = finders.new_table { results = list },
+          sorter = conf.generic_sorter {},
+          previewer = conf.file_previewer {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<leader>hg', function()
+      git_diff()
+    end, { desc = 'Harpoon: Git diff files' })
+
     vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():add()
-    end, { desc = 'Harpood: Add current file' })
+    end, { desc = 'Harpoon: Add current file' })
 
     vim.keymap.set('n', '<leader>h1', function()
       harpoon:list():select(1)
@@ -27,28 +47,8 @@ return {
       harpoon:list():select(4)
     end)
 
-    -- basic telescope configuration
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-        })
-        :find()
-    end
-
     vim.keymap.set('n', '<leader>hh', function()
-      toggle_telescope(harpoon:list())
+      harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Harpoon: Open list' })
   end,
 }
