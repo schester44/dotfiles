43da4183d8e92ab7346256d1590b3f5210967e34
Steve Chester <schester44@gmail.com>
2024-12-24 09:35:47 -0500

    open oil.nvim in float

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,14 +1,18 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name)
+          return name == '.git' or name == '.DS_Store'
+        end,
+      },
+      float = { padding = 2, max_width = 120, max_height = 0 },
+    }
 
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
   end,
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
