218d72b97478d3a6c13574afb5790746ad706f5c
Steve Chester <schester44@gmail.com>
2025-01-08 12:07:20 -0500

    timberrrrrrr

---
+++ nvim/lua/plugins/timber.lua
@@ -0,0 +1,30 @@
+return {
+  'Goose97/timber.nvim',
+  version = '*',
+  event = 'VeryLazy',
+  config = function()
+    vim.keymap.set('n', 'glc', function()
+      require('timber.actions').clear_log_statements { global = false }
+    end, { desc = 'Clear all logs in buffer' })
+
+    require('timber').setup {
+      log_templates = {
+        default = {
+          lua = [[print("%log_marker %log_target", %log_target)]],
+          javascript = [[console.log("%log_marker", {"%log_target": %log_target})]],
+          typescript = [[console.log("%log_marker", {"%log_target": %log_target})]],
+          jsx = [[console.log("%log_marker", {"%log_target": %log_target})]],
+          tsx = [[console.log("%log_marker", {"%log_target": %log_target})]],
+        },
+        plain = {
+          javascript = [[console.log("%log_marker", %insert_cursor)]],
+          typescript = [[console.log("%log_marker", %insert_cursor)]],
+          jsx = [[console.log("%log_marker", %insert_cursor)]],
+          tsx = [[console.log("%log_marker", %insert_cursor)]],
+          lua = [[print("%log_marker", %insert_cursor)]],
+        },
+      },
+      log_marker = '🪵',
+    }
+  end,
+}
