9744f792f46a97ef234ee05267a7c4991195522d
Steve Chester <schester44@gmail.com>
2025-01-13 15:02:50 -0500

    fix tmux keybinds

--- tmux/config/keybinds.sh
+++ tmux/config/keybinds.sh
@@ -12,18 +12,7 @@ tmux bind-key C-a send-prefix
 # tmux session selector
 # https://github.com/joshmedeski/sesh
 # This key is mapped to cmd+k via Wezterm
-bind-key "T" run-shell "sesh connect \"$(
-  sesh list --icons | fzf-tmux -p 55%,60% \
-    --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
-    --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
-    --bind 'tab:down,btab:up' \
-    --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
-    --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
-    --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
-    --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
-    --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-    --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
-)\""
+
 
 # split panes using | and -
 tmux bind-key | split-window -h -c "#{pane_current_path}"
