ff8b9895986e42c56fecaaa61a14c2bda2608193
Steve Chester <schester44@gmail.com>
2025-01-15 16:57:21 -0500

    brew clean up

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -2,30 +2,24 @@
 
 brew analytics off
 
-# Install binaries
-
 brew install postgresql
 brew install fzf
 brew install jq
 brew install kubectl
 
-# Install Apps
-
-## Must haves
 brew install --cask 1password
 brew install --cask 1password-cli
 brew install --cask bitwarden
-brew install --cask authy
-
 brew install --cask google-chrome
-
 brew install --cask spotify
 
 brew install --cask raycast
 brew install --cask syncthing
-brew install --cask rocket
 
-## Dev apps
+# Bash scripting
+brew install shfmt
+brew install shellcheck
+
 brew install --cask postman
 brew install --cask tableplus
 brew install --cask imageoptim
