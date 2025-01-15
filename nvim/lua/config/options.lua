c622c22eac7a2e6a27881d74a5dd8a82a56af85d
Steve Chester <schester44@gmail.com>
2025-01-15 09:32:17 -0500

    set conceallevel in neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -75,3 +75,5 @@ vim.bo.expandtab = true -- Use spaces
 vim.bo.shiftwidth = 2 -- Indent by 2 spaces
 vim.bo.softtabstop = 2 -- Tab inserts 2 spaces
 vim.bo.tabstop = 2 -- Tab width is 2 spaces
+
+vim.opt.conceallevel = 2 -- Conceal text
