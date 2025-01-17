5f7e64a1965436c3bf8e22373350f6a8399374d6
Steve Chester <schester44@gmail.com>
2025-01-17 11:54:36 -0500

    add scratch window keymap

--- nvim/lua/plugins/flowterm.lua
+++ nvim/lua/plugins/flowterm.lua
@@ -5,7 +5,13 @@ return {
     config = function()
       local flowterm = require 'flowterm'
 
-      vim.keymap.set({ 'n', 't' }, '<leader>tt', flowterm.toggle_terminal, { desc = '[T]oggle [T]erminal' })
+      vim.keymap.set({ 'n', 't' }, '<leader>ts', function()
+        flowterm.toggle_floating_win()
+      end, { desc = '[T]oggle [S]cratchpad' })
+
+      vim.keymap.set({ 'n', 't' }, '<leader>tt', function()
+        flowterm.toggle_floating_win 'terminal'
+      end, { desc = '[T]oggle [T]erminal' })
     end,
   },
 }
