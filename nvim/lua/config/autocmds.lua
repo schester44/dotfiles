2b867968425446e9b46cc2dd18437c531073d410
Steve Chester <schester44@gmail.com>
2025-01-17 11:52:49 -0500

    save & restore file info

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,14 +1,44 @@
-local create = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
-create('BufWritePre', {
+local view_group = augroup('auto_view', { clear = true })
+
+-- The following two autocmds are taken from AstroVim.
+-- They are used to save and load the view of a file when it is opened and closed (folds, etc).
+autocmd({ 'BufWinLeave', 'BufWritePost', 'WinLeave' }, {
+  desc = 'Save view with mkview for real files',
+  group = view_group,
+  callback = function(args)
+    if vim.b[args.buf].view_activated then
+      vim.cmd.mkview { mods = { emsg_silent = true } }
+    end
+  end,
+})
+
+autocmd('BufWinEnter', {
+  desc = 'Try to load file view if available and enable view saving for real files',
+  group = view_group,
+  callback = function(args)
+    if not vim.b[args.buf].view_activated then
+      local filetype = vim.api.nvim_get_option_value('filetype', { buf = args.buf })
+      local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+      local ignore_filetypes = { 'gitcommit', 'gitrebase', 'svg', 'hgcommit' }
+      if buftype == '' and filetype and filetype ~= '' and not vim.tbl_contains(ignore_filetypes, filetype) then
+        vim.b[args.buf].view_activated = true
+        vim.cmd.loadview { mods = { emsg_silent = true } }
+      end
+    end
+  end,
+})
+
+autocmd('BufWritePre', {
   desc = 'Fix all eslint issues on save',
   pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
   command = 'silent! EslintFixAll',
   group = augroup('MyAutocmdsJavaScripFormatting', {}),
 })
 
-create('TextYankPost', {
+autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
@@ -16,7 +46,7 @@ create('TextYankPost', {
   end,
 })
 
-create('CmdlineEnter', {
+autocmd('CmdlineEnter', {
   desc = 'Show absolute numbers when entering command-line mode',
   pattern = ':',
   callback = function()
@@ -24,7 +54,7 @@ create('CmdlineEnter', {
   end,
 })
 
-create('CmdlineLeave', {
+autocmd('CmdlineLeave', {
   desc = 'Show relative numbers when leaving command-line mode',
   pattern = ':',
   callback = function()
