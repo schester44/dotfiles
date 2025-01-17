74bfa866b3dc1abefb23100cc370b46c6b3dd9ad
Steve Chester <schester44@gmail.com>
2025-01-17 11:53:51 -0500

    change flow term to allow opening scratch pad or terminal

--- nvim/lua/custom/flowterm.nvim/lua/flowterm/init.lua
+++ nvim/lua/custom/flowterm.nvim/lua/flowterm/init.lua
@@ -24,14 +24,16 @@ local function create_floating_window(opts)
     buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
   end
 
-  local win_config = {
+  local ui = require 'lib.ui'
 
+  local win_config = {
     relative = 'editor',
     width = width,
     height = height,
     col = col,
     row = row,
     style = 'minimal',
+    border = ui.border_chars_outer_thin,
   }
 
   local win = vim.api.nvim_open_win(buf, true, win_config)
@@ -39,11 +41,12 @@ local function create_floating_window(opts)
   return { buf = buf, win = win }
 end
 
-M.toggle_terminal = function()
+-- @param[opt=nil] type the type of buffer, currently only supports terminal
+M.toggle_floating_win = function(type)
   if not vim.api.nvim_win_is_valid(M.state.floating.win) then
     M.state.floating = create_floating_window { buf = M.state.floating.buf }
 
-    if vim.bo[M.state.floating.buf].buftype ~= 'terminal' then
+    if type == 'terminal' and vim.bo[M.state.floating.buf].buftype ~= 'terminal' then
       vim.cmd.terminal()
     end
   else
