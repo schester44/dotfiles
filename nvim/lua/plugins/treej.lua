9380842899018d25f75d0d8393747b57d0ee05ba
Steve Chester <schester44@gmail.com>
2025-01-17 11:54:56 -0500

    add treesj

---
+++ nvim/lua/plugins/treej.lua
@@ -0,0 +1,15 @@
+return {
+  'Wansmer/treesj',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+  config = function()
+    local tj = require 'treesj'
+    tj.setup {
+      use_default_keymaps = false,
+      max_join_length = 999,
+    }
+
+    vim.keymap.set('n', 'gS', function()
+      tj.toggle()
+    end, { desc = 'Treejs - Split/Join', noremap = true, silent = true })
+  end,
+}
