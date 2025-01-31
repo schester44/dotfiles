e2b266e69abf929dfe083770a2849a1ab31640d1
Steve Chester <schester44@gmail.com>
2025-01-30 22:56:39 -0500

    flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,11 +1,37 @@
 return {
-  'folke/flash.nvim',
-  event = 'VeryLazy',
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
+  'folk/flash.nvim',
+  vent = 'VeryLazy',
+
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {
+      'gw',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    {
+      '<c-s>',
+      mode = { 'c' },
+      function()
+        require('flash').toggle()
+      end,
+      desc = 'Toggle Flash Search',
+    },
   },
+  config = function()
+    require('flash').setup {
+      label = { uppercase = false },
+      modes = { char = {
+        highlight = { backdrop = false },
+      } },
+    }
+
+    local palette = require 'cobalt44.palette'
+    vim.api.nvim_set_hl(0, 'FlashMatch', { bg = palette.blue, fg = palette.cobalt_bg_dark })
+    vim.api.nvim_set_hl(0, 'FlashCurrent', { bg = palette.blue, fg = palette.cobalt_bg_dark })
+    vim.api.nvim_set_hl(0, 'FlashLabel', { bg = palette.yellow, fg = palette.cobalt_bg_dark })
+  end,
+  ---@type Flash.Config
 }
