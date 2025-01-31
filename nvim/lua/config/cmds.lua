7e6b53bd56b5aa14b81e809b55647a70cd9994a1
Steve Chester <schester44@gmail.com>
2025-01-30 22:57:54 -0500

    env sync for obie

--- nvim/lua/config/cmds.lua
+++ nvim/lua/config/cmds.lua
@@ -1,3 +1,15 @@
+-- EnvSync for Obie environments
+local target_directory = '/Users/schester/work/risk-management'
+local script_path = target_directory .. '/setup-scripts/sync-environments.sh'
+
+if vim.fn.getcwd() == target_directory then
+  vim.api.nvim_create_user_command('EnvSync', function()
+    print(vim.fn.system(script_path))
+  end, { desc = 'Sync infisical environment' })
+
+  vim.keymap.set('n', '<leader>es', '<CMD>EnvSync<CR>', { desc = '[E]nvironment [S]ync' })
+end
+
 -- Find all unchecked checkboxes in markdown files
 vim.api.nvim_create_user_command('Todos', function()
   vim.fn.setqflist {}
