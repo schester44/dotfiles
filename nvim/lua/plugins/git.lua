d0f1b3b652ab4edb39409fcd6c5b7acf5a61179f
Steve Chester <schester44@gmail.com>
2025-03-06 11:11:04 -0500

    aerospace fixes

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
+  'sindrets/diffview.nvim',
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {},
