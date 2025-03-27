98ba9183b8b052afceffde3827e8bf982a816eb8
Steve Chester <schester44@gmail.com>
2025-03-27 09:33:48 -0400

    add cond for vscode vim

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,9 +1,10 @@
 return {
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-  'sindrets/diffview.nvim',
+  { 'tpope/vim-fugitive', cond = not vim.g.vscode },
+  { 'tpope/vim-rhubarb', cond = not vim.g.vscode },
+  { 'sindrets/diffview.nvim', cond = not vim.g.vscode },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {},
+    cond = not vim.g.vscode,
   },
 }
