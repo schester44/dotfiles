32bf6eec525b7569eae6b8cce09df537065e73ef
Steve Chester <schester44@gmail.com>
2025-11-09 01:19:12 -0500

    remove fugitive, add custom :GBrowse implementation

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -11,65 +11,35 @@ return {
     keys = {
       { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
       {
-        '<leader>gbb',
-        function()
-          require('neogit').action('branch', 'checkout_recent_branch', {})()
-        end,
-        desc = 'Checkout Branch',
-      },
-      {
-        '<leader>gcb',
-        function()
-          require('neogit').action('branch', 'checkout_create_branch', {})()
-        end,
-        desc = 'Create Branch from Current',
+        '<leader>gb',
+        ':Neogit branch<CR>',
+        desc = 'Neogit Branches',
       },
       {
         '<leader>gP',
-        function()
-          require('neogit').action('push', 'to_upstream', {})()
-        end,
-        desc = 'Push Upstream',
+        ':Neogit push<CR>',
+        desc = 'Neogit Push',
       },
-
       {
         '<leader>gp',
-        function()
-          require('neogit').action('pull', 'from_upstream', {})()
-        end,
-        desc = 'Pull Upstream',
+        ':Neogit pull<CR>',
+        desc = 'Neogit Pull',
       },
     },
   },
 
   {
-    'tpope/vim-fugitive',
-    cond = not vim.g.vscode,
-    config = function()
-      set('n', '<leader>go', '<cmd>GBrowse<CR>', { desc = 'GBrowse' })
-      set('x', '<leader>go', ':GBrowse<CR>', { desc = 'GBrowse (selection)' })
-
-      set('n', '<leader>gh', '<cmd>:DiffviewFileHistory %<CR>', { desc = 'File history' })
-      set('n', '<leader>gB', '<cmd>:Git blame<CR>', { desc = 'Git blame' })
-    end,
-  },
-  { 'tpope/vim-rhubarb', cond = not vim.g.vscode },
-  {
     'sindrets/diffview.nvim',
     cond = not vim.g.vscode,
     config = function()
+      set('n', '<leader>gh', '<cmd>:DiffviewFileHistory %<CR>', { desc = 'File history' })
+
       set('n', '<leader>gd', function()
         local ref = vim.fn.input('DiffviewOpen ref/range: ', 'main')
         if ref ~= '' then
           vim.cmd('DiffviewOpen ' .. ref)
         end
       end, { desc = 'Diffview' })
-
-      -- set('n', '<leader>gd', '<cmd>:DiffviewOpen', { desc = 'Git diff' })
-
-      set('n', '<leader>gb', function()
-        Snacks.picker.git_log_line()
-      end, { desc = 'Git blame line' })
     end,
   },
   {
@@ -77,6 +47,8 @@ return {
     opts = {},
     cond = not vim.g.vscode,
     config = function()
+      set('n', '<leader>gB', '<cmd>:Gitsigns blame<CR>', { desc = 'Git blame' })
+
       set('n', ']c', function()
         local gitsigns = require 'gitsigns'
 
