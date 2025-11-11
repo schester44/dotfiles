ac5863a8475728548988201daeba5844203a78bc
Steve Chester <schester44@gmail.com>
2025-11-11 10:05:40 -0500

    biscuits

---
+++ nvim/lua/plugins/biscuits.lua
@@ -0,0 +1,12 @@
+return {
+  'code-biscuits/nvim-biscuits',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {
+    cursor_line_only = true,
+    default_config = {
+      prefix_string = '• ',
+    },
+  },
+}
