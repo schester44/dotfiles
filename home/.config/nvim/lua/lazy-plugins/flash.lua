e5dc2225d4435ba5545eda1e0d8abba5917b3409
Steve Chester <schester44@gmail.com>
2026-07-20 09:56:02 -0400

    flash.nvim keymap improvements

--- home/.config/nvim/lua/lazy-plugins/flash.lua
+++ home/.config/nvim/lua/lazy-plugins/flash.lua
@@ -1,15 +1,31 @@
 return {
   'folke/flash.nvim',
-  vent = 'VeryLazy',
+  event = 'VeryLazy',
 
   keys = {
     {
-      'gw',
+      's',
       mode = { 'n', 'x', 'o' },
       function()
         require('flash').jump()
       end,
       desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Flash Remote',
     },
     {
       '<c-s>',
@@ -23,6 +39,7 @@ return {
   config = function()
     require('flash').setup {
       ---@type Flash.Config
+      highlight = { backdrop = true },
       label = { uppercase = false },
       modes = { char = {
         highlight = { backdrop = false },
@@ -30,8 +47,9 @@ return {
     }
 
     local palette = require 'grapelean.palette'
-    vim.api.nvim_set_hl(0, 'FlashMatch', { bg = palette.blue, fg = palette.bg_dark })
-    vim.api.nvim_set_hl(0, 'FlashCurrent', { bg = palette.blue, fg = palette.bg_dark })
+    vim.api.nvim_set_hl(0, 'FlashBackdrop', { italic = true })
+    vim.api.nvim_set_hl(0, 'FlashMatch', { underline = true, bold = true, fg = palette.pink })
+    vim.api.nvim_set_hl(0, 'FlashCurrent', { underline = true, bold = true, fg = palette.pink_light })
     vim.api.nvim_set_hl(0, 'FlashLabel', { bg = palette.yellow, fg = palette.bg_dark })
   end,
 }
